26db67856799daf999cf0fe6a47ce41ad61dfd37
omea92 <thdjun92@naver.com>
2025-01-07 22:19:14 +0900

    ajax CRUD 컨트롤러 매핑 구현 실습

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.board.entity.Board;
@@ -27,5 +28,28 @@ public class BoardController {
 		List<Board> list = mapper.getLists();
 		
 		return list; //JSON 형식으로 응답
+	}
+	
+	@RequestMapping("/boardInsert.do")
+	public @ResponseBody void boardInsert(Board vo) {
+		mapper.boardInsert(vo); //등록 성공
+	}
+	
+	@RequestMapping("/boardDelete.do")
+	public @ResponseBody void boardDelete(@RequestParam("idx") int idx) {
+		mapper.boardDelete(idx);
+	}
+	
+	@RequestMapping("/boardUpdate.do")
+	public @ResponseBody void boardUpdate(Board vo) {
+		mapper.boardUpdate(vo);
+	}
+	
+	@RequestMapping("/boardContent.do")
+	public @ResponseBody Board boardContent(int idx) {
+		mapper.boardCount(idx);
+		Board vo = mapper.boardContent(idx);
+		
+		return vo;
 	}
 }
