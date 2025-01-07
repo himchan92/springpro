381ecc15723416399991215d64a9a5f6ae56e9c3
omea92 <thdjun92@naver.com>
2025-01-07 21:39:34 +0900

    json responseBody 실습반영

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -1,13 +1,31 @@
 package kr.board.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.board.entity.Board;
+import kr.board.mapper.BoardMapper;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardMapper mapper;
+	
 	@RequestMapping("/")
 	public String main() {
 		return "main";
+	}
+	
+	@RequestMapping("/boardList.do")
+	public @ResponseBody List<Board> boardList() {
+		
+		List<Board> list = mapper.getLists();
+		
+		return list; //JSON 형식으로 응답
 	}
 }
