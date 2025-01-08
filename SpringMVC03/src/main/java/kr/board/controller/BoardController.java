27f2dc10a83193440953b28f4a9b6d92dad74212
omea92 <thdjun92@naver.com>
2025-01-08 22:51:07 +0900

    회원메인페이지 jsp 반영

--- SpringMVC03/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC03/src/main/java/kr/board/controller/BoardController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BoardController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/boardMain.do")
 	public String main() {
-		return "main";
+		return "board/main";
 	}	
 }
