f53bdba3495dae5f69b858f6634b90c154e5e711
omea92 <thdjun92@naver.com>
2025-01-08 23:01:17 +0900

    아이디 중복체크 컨트롤러 반영

--- SpringMVC03/src/main/java/kr/board/controller/MemberController.java
+++ SpringMVC03/src/main/java/kr/board/controller/MemberController.java
@@ -1,13 +1,35 @@
 package kr.board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.board.entity.Member;
+import kr.board.mapper.MemberMapper;
 
 @Controller
 public class MemberController {
 	
+	@Autowired
+	private MemberMapper mapper;
+	
 	@RequestMapping("/memJoin.do")
 	public String memJoin() {
 		return "member/join";
+	}
+	
+	//아이디 중복 체크
+	@RequestMapping("/memRegisterCheck.do")
+	public @ResponseBody int memRegisterCheck(@RequestParam("memID") String memID) {
+		
+		Member m = mapper.registerCheck(memID);
+		
+		if(m != null || memID.equals("")) {
+			return 0; //이미 존재하는 회원
+		}
+		
+		return 1; //사용 가능한 회원
 	}
 }
