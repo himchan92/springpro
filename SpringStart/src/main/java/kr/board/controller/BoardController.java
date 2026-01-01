b4f52333733d6b7e4c8b05b44e7f16ac1ff643e8
omea92 <thdjun92@naver.com>
2026-01-01 17:02:10 +0900

    MVC 실습기본

--- SpringStart/src/main/java/kr/board/controller/BoardController.java
+++ SpringStart/src/main/java/kr/board/controller/BoardController.java
@@ -1,8 +1,35 @@
 package kr.board.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.board.entity.Board;
 
 @Controller
 public class BoardController {
-
+	
+	@RequestMapping("/boardList.do")
+	public String boardList(Model model) {
+		
+		Board vo = new Board();
+		vo.setIdx(1);
+		vo.setTitle("게시판실습");
+		vo.setContent("게시판실습");
+		vo.setWriter("정원준");
+		vo.setIndate("2026-01-01");
+		vo.setCount(0);
+		
+		List<Board> list = new ArrayList<>();
+		list.add(vo);
+		list.add(vo);
+		list.add(vo);
+		
+		model.addAttribute("list", list); //객체바인딩하여 view단에 데이터 전달
+		
+		return "boardList"; // WEB-INF/views/boardList.jsp 연동(servlet-context.xml)
+	}
 }
